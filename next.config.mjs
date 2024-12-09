b4c167a8557f3932b7c179e591f185d6f0575b08
Anmol Agrawal <anmol.ag53@gmail.com>
2024-12-09 19:48:24 +0530

    Added header to supress crossOriginIsolated warning - this increased image processing speed.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
